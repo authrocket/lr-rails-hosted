8ebc69ca0e10ce8f65cc6b776fbc4bdc82ef0ceb
thomas morgan <tm@iprog.com>
2016-07-29 13:52:41 -0600

    upgrade to rails 5

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_lr-rails-hosted_session'
+Rails.application.config.session_store :cookie_store, key: '_lr_rails_hosted_session'
